c10b9f93cc38b68eff70488bde94bb551cc3ff7b
Fabio <bilotto@github.com>
2024-05-22 14:22:40 -0300

    Refactor example scripts to use external peers for PCEF and PCRF nodes

--- examples/pcrf.py
+++ examples/pcrf.py
@@ -1,21 +1,21 @@
-from diameter.node import Node
-from diameter.message.constants import *
+# from diameter.node import Node
+from DiamTelecom.diameter.constants import *
 from DiamTelecom.diameter.app import *
+from DiamTelecom.handle_request import handle_request
 from typing import List, Dict
+
 import logging
-# this shows a human-readable message dump in the logs
 logging.basicConfig(format="%(asctime)s %(name)-22s %(levelname)-7s %(message)s",
                     level=logging.DEBUG)
-
-# this shows a human-readable message dump in the logs
-logging.getLogger("diameter.peer.msg").setLevel(logging.DEBUG)
-
-from DiamTelecom.diameter.create_nodes import *
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
+    # this shows a human-readable message dump in the logs
+    logging.getLogger("diameter.peer.msg").setLevel(logging.DEBUG)
+
     pcrf_node = create_node("pcrf", "example.com", ["127.0.0.1"], 3868)
 
-    pcrf_peers_list = [
+    pcrf_external_peers_list = [
         {
             "host": "pcef",
             "port": 3869,
@@ -25,8 +25,8 @@ if __name__ == "__main__":
             "is_default": False
         }
     ]
-    pcrf_peers = add_peers(pcrf_node, pcrf_peers_list)
-    pcrf = create_gx_app(10)
+    pcrf_peers = add_peers(pcrf_node, pcrf_external_peers_list)
+    pcrf = create_gx_app(10, handle_request)
     pcrf_node.add_application(pcrf, pcrf_peers)
 
     pcrf.node.vendor_id = VENDOR_TGPP
--- src/DiamTelecom/diameter/app.py
+++ src/DiamTelecom/diameter/app.py
@@ -1,24 +1,45 @@
+from diameter.node import Node
+from diameter.node.node import Peer
 from diameter.node.application import SimpleThreadingApplication
 from .session import *
+from typing import List, Dict
 
 class CustomSimpleThreadingApplication(SimpleThreadingApplication):
     def __init__(self, application_id, is_acct_application, is_auth_application, max_threads, request_handler):
         super().__init__(application_id, is_acct_application, is_auth_application, max_threads, request_handler)
         self.sessions = DiameterSessions()
+        self.started = False
 
     def get_session_by_id(self, session_id: str) -> DiameterSession:
         return self.sessions.get_diameter_session(session_id)
     
     def get_subscriber_sessions_by_msisdn(self, msisdn: int):
         return self.sessions.get_msisdn_sessions(msisdn)
+    
+    def get_subscriber_active_session(self, msisdn: int):
+        if self.sessions.get_msisdn_sessions(msisdn):
+            for session in self.sessions.get_msisdn_sessions(msisdn):
+                if session.active:
+                    return session
         
+    def custom_start(self):
+        if not self.started:
+            self.node.start()
+            self.wait_for_ready()
+            self.started = True
+
+    def custom_stop(self):
+        if self.started:
+            self.node.stop()
+            self.started = False
+
 class GxApplication(CustomSimpleThreadingApplication):
     sessions: GxSessions
     def __init__(self, application_id, is_acct_application, is_auth_application, max_threads, request_handler):
         super().__init__(application_id, is_acct_application, is_auth_application, max_threads, request_handler)
         self.sessions = GxSessions()
 
-class RxAppication(CustomSimpleThreadingApplication):
+class RxApplication(CustomSimpleThreadingApplication):
     sessions: RxSessions
     def __init__(self, application_id, is_acct_application, is_auth_application, max_threads, request_handler):
         super().__init__(application_id, is_acct_application, is_auth_application, max_threads, request_handler)
@@ -29,4 +50,28 @@ class SyApplication(CustomSimpleThreadingApplication):
     def __init__(self, application_id, is_acct_application, is_auth_application, max_threads, request_handler):
         super().__init__(application_id, is_acct_application, is_auth_application, max_threads, request_handler)
         self.sessions = SySessions()
-        self.subscribers = None 
+        self.subscribers = None
+
+
+def create_node(origin_host, realm, ip_addresses, tcp_port) -> Node:
+    node = Node(origin_host, realm, ip_addresses=ip_addresses, tcp_port=tcp_port, vendor_ids=[VENDOR_ETSI, VENDOR_TGPP, VENDOR_TGPP2])
+    node.idle_timeout = 20
+    return node
+
+def add_peers(node: Node, peers_list: List[Dict]) -> List[Peer]:
+    return [node.add_peer(f"aaa://{peer['host']}:{peer['port']};transport=tcp",
+                          peer['realm'],
+                          ip_addresses=peer.get('ip_addresses'),
+                          is_persistent=peer['is_persistent'],
+                          is_default=peer.get('is_default', False))
+            for peer in peers_list]
+
+
+def create_gx_app(max_threads, request_handler) -> GxApplication:
+    return GxApplication(APP_3GPP_GX, is_acct_application=False, is_auth_application=True, max_threads=max_threads, request_handler=request_handler)
+
+def create_rx_app(max_threads, request_handler) -> RxApplication:
+    return RxApplication(APP_3GPP_RX, is_acct_application=False, is_auth_application=True, max_threads=max_threads, request_handler=request_handler)
+
+def create_sy_app(max_threads, request_handler) -> SyApplication:
+    return SyApplication(APP_3GPP_SY, is_acct_application=False, is_auth_application=True, max_threads=max_threads, request_handler=request_handler)
