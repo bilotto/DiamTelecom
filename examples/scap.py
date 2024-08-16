fe4784935f65126c52f14291f490ab449115a237
Fabio Bilotto <fabio.bilotto@openet.com>
2024-08-16 15:46:38 -0300

    dev

--- examples/scap.py
+++ examples/scap.py
@@ -13,7 +13,29 @@ logging.getLogger("diameter.peer.msg").setLevel(logging.DEBUG)
 from DiamTelecom.diameter.create_nodes import *
 
 def handle_request(app: CustomSimpleThreadingApplication, message: Message):
-    pass
+    answer = message.to_answer()
+    session_id = message.session_id
+    print(f"Received message {message} with session_id {session_id}")
+    try:
+        answer.session_id = message.session_id
+        answer.origin_host = app.node.origin_host.encode()
+        answer.origin_realm = app.node.realm_name.encode()
+        answer.destination_realm = message.origin_realm
+        answer.cc_request_type = E_CC_REQUEST_TYPE_EVENT_REQUEST
+        answer.cc_request_number = message.cc_request_number
+        answer.result_code = E_RESULT_CODE_DIAMETER_SUCCESS
+        if isinstance(answer, CreditControlAnswer):
+            answer.cost_information = CostInformation()
+            answer.cost_information.unit_value = UnitValue()
+            answer.cost_information.unit_value.value_digits = 100
+            answer.cost_information.unit_value.exponent = 6
+            answer.cost_information.currency_code = 986
+
+    except Exception as e:
+        print(f"Error processing message: {e}")
+        answer.result_code = E_RESULT_CODE_DIAMETER_UNABLE_TO_DELIVER
+    return answer
+
 
 if __name__ == "__main__":
     import os
@@ -37,7 +59,7 @@ if __name__ == "__main__":
         }
     ]
     pcef_peers = add_peers(pcef_node, pcef_peers_list)
-    pcef = create_gx_app(10, handle_request)
+    pcef = create_gy_app(10, handle_request)
     pcef_node.add_application(pcef, pcef_peers)
     pcef.node.vendor_id = VENDOR_TGPP
     pcef.node.product_name = "PCEF"
