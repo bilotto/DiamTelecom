aaf51bd9b32226c158bcb7596923f817ff754f45
Fabio <bilotto@github.com>
2024-09-20 16:14:24 -0300

    Refactor start method in DiameterApplications class to call custom_start method for each app and handle exceptions

--- src/DiamTelecom/diameter/app.py
+++ src/DiamTelecom/diameter/app.py
@@ -83,8 +83,14 @@ class DiameterApplications:
         return list(self.apps_per_id.values())
     
     def start(self):
-        for node in self.nodes:
-            node.start()
+        try:
+            for app in self.apps:
+                app.custom_start()
+        except Exception as e:
+            print(e)
+            pass
+        # for node in self.nodes:
+        #     node.start()
 
     def wait_for_ready(self):
         for app in self.apps:
