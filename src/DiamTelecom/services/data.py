24c2b4a82137877bc79bd69414d8d48745a59cdd
Fabio <bilotto@github.com>
2024-09-21 10:21:42 -0300

    Refactor DataService class to include a logger for logging messages

--- src/DiamTelecom/services/data.py
+++ src/DiamTelecom/services/data.py
@@ -30,6 +30,7 @@ class DataService():
         self._realm = None
         self._mcc_mnc = None
         self._apn = None
+        self.logger = logging.getLogger(__name__)
 
     @property
     def realm(self) -> str:
@@ -101,12 +102,16 @@ class DataService():
         sy_session = self.sy_service.sy_app.get_subscriber_active_session(subscriber.msisdn)
         if not sy_session:
             return None
+        self.logger.info(f"SY Session found for subscriber {subscriber}: {sy_session}")
         return sy_session
 
     def send_policy_counter_status_report(self, sy_session: SySession, policy_counter_dict, wait_raa=True):
         logger.info(f"Sending SSN Request: {policy_counter_dict}")
         ssnr = self.sy_service.create_ssnr(sy_session, policy_counter_dict)
         ssna = self.sy_service.send_sy_request(sy_session, ssnr)
+        if self.realm:
+            ssna.destination_realm = self.realm.encode()
+        #
         if wait_raa:
             self.gx_service.wait_for_gx_raa(gx_session, len(gx_session.messages), timeout=5)
 
