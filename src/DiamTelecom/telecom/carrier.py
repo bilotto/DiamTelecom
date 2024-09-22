0cb33f99987cfe32f3e65ce3a8d846e3006d1f18
Fabio <bilotto@github.com>
2024-09-22 11:35:56 -0300

    Refactor Carrier class to include generate_subscribers method

--- src/DiamTelecom/telecom/carrier.py
+++ src/DiamTelecom/telecom/carrier.py
@@ -13,7 +13,6 @@ class Carrier:
     data_service: DataService
     subscribers: Subscribers
     
-
     def __init__(self,
                  name,
                  carrier_id,
@@ -25,39 +24,28 @@ class Carrier:
         self.carrier_id = carrier_id
         self.mcc_mnc = mcc_mnc
         self.country_code = country_code
-        self.data_service = None
         self.msisdn_template = f"{str(country_code)}0000000"
         self.imsi_template = f"{str(mcc_mnc)}000000000"[:15]
-
         if subscribers:
             self.subscribers = subscribers
         else:
             self.subscribers = Subscribers()
-            # self.subscribers = generate_subscribers(self.subscribers, self.mcc_mnc, self.country_code, 1000)
-            self.subscribers = generate_subscribers(self.subscribers, self.msisdn_template, self.imsi_template, self.carrier_id, 100)
         #
+        self.data_service = None
         self.voice_service = None
 
-
-
     def set_voice_service(self, voice_service: VoiceService):
+        voice_service.set_mcc_mnc(self.mcc_mnc)
+        voice_service.set_apn("ims")
         self.voice_service = voice_service
 
     def set_data_service(self, data_service: DataService):
+        data_service.set_mcc_mnc(self.mcc_mnc)
         self.data_service = data_service
 
-    def start_voice_service(self):
-        if self.voice_service:
-            self.voice_service.start()
-
-    def stop_voice_service(self):
-        if self.voice_service:
-            self.voice_service.stop()
-
-    def start_data_service(self):
-        if self.data_service:
-            self.data_service.start()
-
-    def stop_data_service(self):
-        if self.data_service:
-            self.data_service.stop()
+    def generate_subscribers(self, count: int):
+            self.subscribers = generate_subscribers(self.subscribers,
+                                                    self.msisdn_template,
+                                                    self.imsi_template,
+                                                    self.carrier_id,
+                                                    count)
