e6ab756c325cf247fb428e6e010054b2c866e5c6
Fabio <bilotto@github.com>
2024-09-22 18:23:47 -0300

    Refactor DiameterApplications class to import SyApplication and DiameterApplications

--- src/DiamTelecom/diameter/apps/__init__.py
+++ src/DiamTelecom/diameter/apps/__init__.py
@@ -4,3 +4,4 @@ from .gx_app_ import GxApplication
 from .gy_app_ import GyApplication
 from .rx_app_ import RxApplication
 from .sy_app_ import SyApplication
+from .diameter_applications import DiameterApplications
