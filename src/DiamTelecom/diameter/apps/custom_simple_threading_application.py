2284f8f384b7fe81d6cbe46902ad80c537259248
Fabio <bilotto@github.com>
2024-09-23 01:38:35 -0300

    Refactor DiameterSession class to handle session start and end times Refactor GxSession class to remove unused code Refactor VoiceService class to handle GX session creation and activation: Handle inactive GX session when starting voice session Refactor VoiceService class to handle GX session creation and activation Refactor DataService and VoiceService classes to add a method for stopping all GX sessions Add DiameterStatistics class to track request counts and result codes Update CustomSimpleThreadingApplication to use DiameterStatistics for tracking request counts and result codes Update CustomSimpleThreadingApplication to increment request counts and result codes in send_request_custom method

--- src/DiamTelecom/diameter/apps/custom_simple_threading_application.py
+++ src/DiamTelecom/diameter/apps/custom_simple_threading_application.py
@@ -1,6 +1,26 @@
 from diameter.node.application import SimpleThreadingApplication, Node
 from ..sessions import DiameterSessions, DiameterSession
 
+class DiameterStatistics:
+    def __init__(self):
+        self.request_count = dict()
+        self.request_count['success'] = 0
+        self.request_count['failure'] = 0
+        self.rc_count = dict()
+
+    def increment_request_count(self, success: bool):
+        if success:
+            self.request_count['success'] += 1
+        else:
+            self.request_count['failure'] += 1
+
+    def increment_rc_count(self, rc):
+        if rc not in self.rc_count:
+            self.rc_count[rc] = 1
+        else:
+            self.rc_count[rc] += 1
+
+
 class CustomSimpleThreadingApplication(SimpleThreadingApplication):
     def __init__(self, application_id,
                  is_acct_application,
@@ -10,6 +30,7 @@ class CustomSimpleThreadingApplication(SimpleThreadingApplication):
                  ):
         super().__init__(application_id, is_acct_application, is_auth_application, max_threads, request_handler)
         self.sessions = DiameterSessions()
+        self.stats = DiameterStatistics()
 
     def get_session_by_id(self, session_id: str) -> DiameterSession:
         return self.sessions.get_session(session_id)
@@ -26,6 +47,10 @@ class CustomSimpleThreadingApplication(SimpleThreadingApplication):
     def send_request_custom(self, request, timeout=5):
         try:
             answer = self.send_request(request, timeout)
+            result_code = answer.result_code
+            self.stats.increment_rc_count(result_code)
+            self.stats.increment_request_count(success=True)
             return answer
         except Exception as e:
+            self.stats.increment_request_count(success=False)
             raise e
