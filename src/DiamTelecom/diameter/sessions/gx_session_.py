fb0b0b04acf026b93f6d76dadf433714b85e6fb5
Fabio <bilotto@github.com>
2024-09-24 15:22:40 -0300

    Refactor GxSession class __repr__ method to include additional session details

--- src/DiamTelecom/diameter/sessions/gx_session_.py
+++ src/DiamTelecom/diameter/sessions/gx_session_.py
@@ -29,7 +29,11 @@ class GxSession(DiameterSession):
         self.rx_sessions = []
 
     def __repr__(self):
-        return f"GxSession(msisdn={self.msisdn},session_id={self.session_id},\nactive={self.active},\nn_messages={self.n_messages},\nlast_message={self.last_message})"
+        return f"GxSession(msisdn={self.msisdn},
+        \n          session_id={self.session_id},
+        \n          active={self.active},
+        \n          n_messages={self.n_messages},
+        \n          last_message={self.last_message})"
 
     def incr_cc_request_number(self):
         self.cc_request_number += 1
