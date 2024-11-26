8a2695a8ff5ddc3619913f1d0001dd262aa85db1
Fabio <fbilotto@amdocs.com>
2024-11-26 13:52:49 -0300

    Remove PCRFService class and its associated file from the project

--- src/DiamTelecom/services/pcrf.py
+++
@@ -1,8 +0,0 @@
-from .data import DataService
-
-
-class PCRFService(DataService):
-    def __init__(self, gx_service, sy_service):
-        super().__init__(gx_service, sy_service)
-        self.gx_
-
